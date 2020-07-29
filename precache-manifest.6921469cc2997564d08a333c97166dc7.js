d047bbb197b435f053429cf849e74b63e8cc5fae
Aleksandar <aleksandar.naskovich@gmail.com>
2020-07-29 21:10:00 +0200

    Updates

--- precache-manifest.6921469cc2997564d08a333c97166dc7.js
+++ precache-manifest.6921469cc2997564d08a333c97166dc7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c33d14ece0a48bf014c558ecb2d0d914",
+    "revision": "ca470a84b43571983de0f394dc61742b",
     "url": "/tic-tac-toe/index.html"
   },
   {
-    "revision": "70203c5e9d09a5d50ee1",
+    "revision": "fad929a34e46f2dac46c",
     "url": "/tic-tac-toe/static/css/main.34b1e410.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tic-tac-toe/static/js/2.297402d2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "70203c5e9d09a5d50ee1",
-    "url": "/tic-tac-toe/static/js/main.6b011217.chunk.js"
+    "revision": "fad929a34e46f2dac46c",
+    "url": "/tic-tac-toe/static/js/main.1a63bf38.chunk.js"
   },
   {
     "revision": "7f32745e7b69030faee7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tic-tac-toe/precache-manifest.b1414b487bd5291447cd9cba0fd166a5.js"
+  "/tic-tac-toe/precache-manifest.6921469cc2997564d08a333c97166dc7.js"
 );
 
 self.addEventListener('message', (event) => {
