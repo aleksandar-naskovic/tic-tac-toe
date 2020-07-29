502e3aad812d454a0848674fd22b5421d53547df
Aleksandar <aleksandar.naskovich@gmail.com>
2020-07-29 20:53:45 +0200

    Updates

---
+++ precache-manifest.b1414b487bd5291447cd9cba0fd166a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33d14ece0a48bf014c558ecb2d0d914",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "70203c5e9d09a5d50ee1",
+    "url": "/tic-tac-toe/static/css/main.34b1e410.chunk.css"
+  },
+  {
+    "revision": "78d7a98922dd7ec37356",
+    "url": "/tic-tac-toe/static/js/2.297402d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.297402d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70203c5e9d09a5d50ee1",
+    "url": "/tic-tac-toe/static/js/main.6b011217.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
